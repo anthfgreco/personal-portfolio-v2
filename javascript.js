8b7e3ba86364d86994120ceb127c6697a21facef
popTheSmart <popthesmart@hotmail.com>
2021-05-17 11:27:09 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var currentHour = (new Date()).getHours();
+
+// Morning 0-11
+if (currentHour <= 11) {
+  document.getElementById("greeting").innerHTML = "☕ Good Morning 🌅";
+}
+// Afternoon 12-16
+else if (currentHour <= 16) {
+  document.getElementById("greeting").innerHTML = "😎 Good Afternoon 🌞";
+}
+// Evening 17-20
+else if (currentHour <= 20) {
+  document.getElementById("greeting").innerHTML = "🌆 Good Evening 🌇";
+}
+// Night 21-23
+else {
+  document.getElementById("greeting").innerHTML = "🌚 Hello 🌃";
+}
