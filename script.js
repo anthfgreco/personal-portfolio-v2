13173276d6e603ad231b4417d9e56474684e06c7
popTheSmart <popthesmart@hotmail.com>
2022-09-20 16:42:22 -0400

    fix bug where greeting is hidden + update text

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 // Face background: #dbc7ff
 
-$(function() {
-  let currentHour = (new Date()).getHours();
-  let greeting = "#greeting";
+let currentHour = (new Date()).getHours();
+let greeting_id = "#greeting";
 
-  // Night 0-4
-  if (currentHour <= 4)       $(greeting).text("🌚Hello🌃");
-  // Morning 5-10
-  else if (currentHour <= 10) $(greeting).text("☕Good Morning🌅");
-  // Afternoon 11-16
-  else if (currentHour <= 16) $(greeting).text("😎Good Afternoon🌞");
-  // Evening 17-20
-  else if (currentHour <= 20) $(greeting).text("🌆Good Evening🌇");
-  // Night 21-23
-  else                        $(greeting).text("🌚Hello🌃");
-});
+// Night 0-4
+if (currentHour <= 4)       $(greeting_id).text("🌚Hello🌃");
+// Morning 5-10
+else if (currentHour <= 10) $(greeting_id).text("☕Good Morning🌅");
+// Afternoon 11-16
+else if (currentHour <= 16) $(greeting_id).text("😎Good Afternoon🌞");
+// Evening 17-20
+else if (currentHour <= 20) $(greeting_id).text("🌆Good Evening🌇");
+// Night 21-23
+else                        $(greeting_id).text("🌚Hello🌃");
