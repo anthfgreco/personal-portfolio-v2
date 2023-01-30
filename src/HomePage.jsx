8dddd0188afc3de9066f0951bb539b174ffe789f
popTheSmart <anthony1.greco@ryerson.ca>
2023-01-29 22:40:01 -0500

    refactor HomePage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,6 +3,36 @@ import { useEffect } from "react";
 import SocialMediaNavBar from "./SocialMediaNavBar";
 import { EMAIL, MAILTO_EMAIL } from "./global";
 
+const emailLink = (
+  <a
+    href={MAILTO_EMAIL}
+    target="_blank"
+    className="text-violet-500 hover:text-violet-900"
+  >
+    {EMAIL}
+  </a>
+);
+
+const paragraphList = [
+  <>
+    I'm a Computer Science graduate from Toronto Metropolitan University in
+    Toronto, Ontario.
+  </>,
+  <>
+    I'm passionate about finding creative ways to solve problems, especially
+    through technology.
+  </>,
+  <>
+    Most of my personal projects are available on my GitHub, linked below. I'm
+    currently learning and making projects with web technologies such as React,
+    Tailwind CSS, Flask, and Django.
+  </>,
+  <>
+    I'm currently looking for a position related to software engineering or
+    machine learning. Reach me at {emailLink}, even for a chat!
+  </>,
+];
+
 function HomePage() {
   useEffect(() => {
     document.title = "Anthony Greco";
@@ -10,34 +40,13 @@ function HomePage() {
 
   return (
     <div>
-      <p className="mb-5 text-[#202020] dark:text-[#ededed]">
-        I'm a Computer Science graduate from Toronto Metropolitan University in
-        Toronto, Ontario.
-      </p>
-
-      <p className="mb-5 text-[#202020] dark:text-[#ededed]">
-        I'm passionate about finding creative ways to solve problems, especially
-        through technology.
-      </p>
-
-      <p className="mb-5 text-[#202020] dark:text-[#ededed]">
-        Most of my personal projects are available on my GitHub, linked below.
-        I'm currently learning and making projects with web technologies such as
-        React, Tailwind CSS, Flask, and Django.
-      </p>
-
-      <p className="mb-5 text-[#202020] dark:text-[#ededed]">
-        I'm currently looking for a position related to software engineering or
-        machine learning. Reach me at{" "}
-        <a
-          href={MAILTO_EMAIL}
-          target="_blank"
-          className="text-violet-500 hover:text-violet-900"
-        >
-          {EMAIL}
-        </a>
-        , even for a chat!
-      </p>
+      {paragraphList.map((paragraph, i) => {
+        return (
+          <p key={i} className="mb-5 text-[#202020] dark:text-[#ededed]">
+            {paragraph}
+          </p>
+        );
+      })}
 
       <br />
 
